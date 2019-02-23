bda83ccb07d1d95034db9a08e76fb4c62f75d823
Scratch <Scrxtchy@users.noreply.github.com>
2019-02-23 23:02:12 +1100

    Update yama.py

--- yama.py
+++ yama.py
@@ -5,7 +5,10 @@ key = "" #https://xivapi.com/app
 
 x = requests.get("https://www.garlandtools.org/db/doc/item/en/3/24988.json").json()
 
-for item in x['item']['tradeShops'][0]['listings']:
-	y = requests.get("https://xivapi.com/market/cactuar/items/{0}?key={1}".format(str(item['currency'][0]['id']), key)).json()
-	print(item['currency'][0]['id'] + "cost: {}".format(y['Prices'][0]['PricePerUnit']) )
+for item in range(23768, 23783):
+	y = requests.get("https://xivapi.com/market/cactuar/items/{0}?key={1}".format(str(item), key)).json()
+	for z in y['Prices']:
+		if z['IsHQ']:
+			print(y['Item']['Name'] + ": {}".format(z['PricePerUnit']) )
+			break
 sleep(0.5)
